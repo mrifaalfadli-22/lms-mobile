4e989040b362d3c2bbcd71d72928b534b7d73aa0
rifaalfadli <rifaalfadli11@gmail.com>
2026-07-05 23:16:34 +0700

    feat: add API_BASE_URL configuration with default environment fallback

--- config/api.js
+++ config/api.js
@@ -1,2 +1,2 @@
 // config/api.js
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://lms-ucademy.duckdns.org:8010/api';
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://lms-ucademy.duckdns.org:8010';
