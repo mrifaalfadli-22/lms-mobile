974677629ada7dfa39b99a4533a14f06c04966ed
rifaalfadli <rifaalfadli11@gmail.com>
2026-07-05 23:01:57 +0700

    feat: add configuration for API base URL with environment variable support

--- config/api.js
+++ config/api.js
@@ -1,2 +1,2 @@
 // config/api.js
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.101.14:8000';
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://lms-ucademy.duckdns.org:8010/api';
